70b1a809fe5b7f5ef71038060ea4b62c4bc0b667
Mark Agimba <markaguero16@gmail.com>
2019-04-29 11:38:15 +0300

    create app function

--- app/__init__.py
+++ app/__init__.py
@@ -28,8 +28,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    mail.init_app(app)
     simple.init_app(app)
+    mail.init_app(app)
 
     # Blueprint registration
     from .main import main as main_blueprint
