0aaa035e3b95fca3bd8e0e8e81852fb238b6c6fe
Mark Agimba <markaguero16@gmail.com>
2019-04-29 11:30:45 +0300

    define imports

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,12 @@ from flask_mail import Mail
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_simplemde import SimpleMDE
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+simple = SimpleMDE()
+photos = UploadSet('photos',IMAGES)
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
